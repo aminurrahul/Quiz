d52fa47f8067258b80ea946cc51213aa19948a4f
AminuR Rasul RahuL <lidorara@gmail.com>
2021-09-22 17:31:17 -0400

    second commit

---
+++ game.js
@@ -0,0 +1,38 @@
+var questionTitle = document.getElementById('question');
+var choice1 = document.getElementById('');
+var questionIndex = 0;
+var currentQuestion = questions[questionIndex];
+
+var questions = [
+    {
+        question: "sample",
+        choices: ['1','2','3'],
+        correct: ['1']
+    },
+    {
+        question: "sample",
+        choices: ['1','2','3'],
+        correct: ['1']
+    },
+    {
+        question: "sample",
+        choices: ['1','2','3'],
+        correct: ['1']
+    },
+    {
+        question: "sample",
+        choices: ['1','2','3'],
+        correct: ['1']
+    },
+    {
+        question: "sample",
+        choices: ['1','2','3'],
+        correct: ['1']
+    }
+]
+
+function start() {
+    // write code to display current questions and answers
+    // write code to start timer countdown
+    // write another function to handle answer click, then display next question
+}
